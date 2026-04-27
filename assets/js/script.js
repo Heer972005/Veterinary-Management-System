a2f39911c87ffb32ac0ddb091d8b6d00942fa362
Heer972005 <mehtaheer.972@gmail.com>
2026-04-27 14:13:17 +0530

    done with folder stucture

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    console.log("JS Loaded");
+
+    // Example: alert on click
+    $(".card").click(function () {
+        alert("Feature coming soon!");
+    });
+
+});
